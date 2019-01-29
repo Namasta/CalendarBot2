1bebff752b768a1a940dafafda6eefd3cb2970b8
Namasta <fcchoong@gmail.com>
2019-01-29 17:20:07 +0800

    .

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
 
+
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./serviceAccountKey.json");
@@ -21,7 +22,19 @@ admin.initializeApp({
   databaseURL: "https://calendarbot2-e0f84.firebaseio.com"
 });
 
+const {
+    dialogflow,
+    Image,
+    Table,
+    Carousel,
+  } = require('actions-on-google');
+  var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+  var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+  var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
 const app = dialogflow();
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 //admin.initializeApp();
 const db = admin.firestore();
