10ee4bb1ab7294fb708795d8cdb3d1bdfb0ee055
Namasta <fcchoong@gmail.com>
2019-01-29 17:22:43 +0800

    .

--- index.js
+++ index.js
@@ -36,11 +36,41 @@ const app = dialogflow();
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-//admin.initializeApp();
 const db = admin.firestore();
 
 const timeZone = 'Asia/HongKong';
 const timeZoneOffset = '+08:00';
+
+
+app.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`);
+    }
+
+    function fallback(agent) {
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
+
+    function testImage(agent) {
+        agent.add(`I am testimage`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
+
+
+
+    // Run the proper function handler based on the matched Dialogflow intent name
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('GetImageDetailIntent', testImage);
+    // intentMap.set('your intent name here', googleAssistantHandler);
+    agent.handleRequest(intentMap);
+});
 
 const expressApp = express().use(bodyParser.json());
 
