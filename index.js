c9bbec51ace40f350450c71506d699609ecd38e8
Namasta <fcchoong@gmail.com>
2019-01-29 16:20:50 +0800

    .

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+'use strict';
+
 const functions = require('firebase-functions');
 const {webhookClient,  Button, Suggestion } = require('dialogflow-fulfillment');
 const {dialogflow,BasicCard,} = require('actions-on-google');
 const requestLib = require('request');
 
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://calendarbot2-e0f84.firebaseio.com"
+});
+
 const app = dialogflow();
-const admin = require('firebase-admin');//init db
+
 admin.initializeApp();
 const db = admin.firestore();
 
@@ -262,7 +280,16 @@ function updateParam(email,number,param,date,time,location,duration,conv){
     });
 }
 
+const expressApp = express().use(bodyParser.json());
 
+expressApp.post('/fulfillment', app.dialogflowFirebaseFulfillment);
+//expressApp.listen(3000);
+var listener = expressApp.listen(process.env.PORT, process.env.IP, function () {
+    //var listener = app.listen(4000, process.env.IP, function () {
+    console.log("server started");
+    console.log("listening on port " +
+      listener.address().port);
+  });
 
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
