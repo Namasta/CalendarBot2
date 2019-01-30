53adea28e1159dc342e8821daf5666d669d06d10
Namasta <fcchoong@gmail.com>
2019-01-30 11:47:32 +0800

    .

--- index.js
+++ index.js
@@ -59,13 +59,13 @@ app.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response
     function createAppointment(agent) {
         agent.add(`I am testimage`);
         agent.add(`I'm sorry, can you try again?`);
-        var entry = {   name: agent.parameters.name,
-            email:agent.parameters.email,
-            date :agent.parameters.date,
-            time:agent.parameters.time,
-            location: agent.parameters.location,
-            duration:agent.parameters.duration,
-            event:agent.parameters.event};            
+           name: agent.parameters.name;
+            email:agent.parameters.email;
+            date :agent.parameters.date;
+            time:agent.parameters.time;
+            location: agent.parameters.location;
+            duration:agent.parameters.duration;
+            event:agent.parameters.event ;         
 
        // createappt(conv,entry);
        agent.add('Done! '+ name +' , I have recorded that you have a ' + event + ' on ' + date.split('T')[0] + ' at ' + time.split('T')[1].split('+')[0] + ' for '+ duration[0] + ' under '+ email +'.');
